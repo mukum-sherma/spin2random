5ff14f569b59d2eefbf06b6fe3340749f6564f30
mukum-sherma <mukum.sherma@gmail.com>
2025-12-09 11:54:56 +0800

    ADDED A WARNING DIALOG FOR AN EMPTY NAMES LIST  , ADDED GOOGLE AD SENSE --V36 ---

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,10 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				<meta
+					name="google-adsense-account"
+					content="ca-pub-9815804917269409"
+				></meta>
 				<script
 					async
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9815804917269409"
